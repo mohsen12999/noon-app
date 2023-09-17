d75488b0b21d06dd772a91d7b7c2857953a75f9d
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-09-17 20:48:29 +0330

    fix page after change data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,63 +6,64 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Link from 'next/link';
 
-import styles from './page.module.css'
+import { GROUPS } from '@/helper/data';
+import { IGroup } from '@/helper/interfaces';
 
-import { IProductGroup, PRODUCT_GROUPS } from "../helper/data";
+import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Container maxWidth="md">
-      <Grid
-        className={styles.grid}
-        container
-        spacing={3}
-        // justify="space-around"
-        alignItems="stretch"
-      >
-        {PRODUCT_GROUPS.map((group: IProductGroup) => (
-          <Grid
-            className={styles.littleGrid}
-            key={group.id}
-            item
-            xs={12}
-            sm={6}
-          >
-            <Link href={group.enable? ("/market/"+group.id) :"/soon"} className='noTextDecoration'>
+        <Grid
+          className={styles.grid}
+          container
+          spacing={3}
+          // justify="space-around"
+          alignItems="stretch"
+        >
+          {GROUPS.map((group: IGroup) => (
+            <Grid
+              className={styles.littleGrid}
+              key={group.id}
+              item
+              xs={12}
+              sm={6}
+            >
+              <Link href={group.enabled ? ("/" + group.id) : "/soon"} className='noTextDecoration'>
 
-              <Card className={styles.card}>
-                <CardMedia
-                  className={group.enable? styles.cover:styles.disableCover }
-                  image={group.img}
-                  title={group.persianSubtitle}
-                />
+                <Card className={styles.card}>
+                  <CardMedia
+                    className={group.enabled ? styles.cover : styles.disableCover}
+                    image={group.img}
+                    title={group.persianSubtitle}
+                  />
 
-                <div className={styles.details}>
-                  <CardContent className={styles.content}>
-                    <Typography
-                      className="YekanFont"
-                      component="h5"
-                      variant="h5"
-                    >
-                      {group.persianTitle}
-                    </Typography>
-                    <Typography
-                      className="YekanFont"
-                      variant="subtitle1"
-                      color="textSecondary"
-                    >
-                      {group.persianSubtitle}
-                    </Typography>
-                  </CardContent>
-                </div>
-              </Card>
+                  <div className={styles.details}>
+                    <CardContent className={styles.content}>
+                      <Typography
+                        className="YekanFont"
+                        component="h5"
+                        variant="h5"
+                      >
+                        {group.persianTitle}
+                      </Typography>
+                      <Typography
+                        className="YekanFont"
+                        variant="subtitle1"
+                        color="textSecondary"
+                      >
+                        {group.persianSubtitle}
+                      </Typography>
+                    </CardContent>
+                  </div>
+                </Card>
 
-            </Link>
+              </Link>
 
-          </Grid>
-        ))}
-      </Grid>
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </main>
   )
