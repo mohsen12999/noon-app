1cef49756885c35d5370b08f4810d79775d1f141
mohsen1299@gmail.com <mohsen1299@gmail.com>
2023-10-01 11:21:45 +0330

    fix data information

--- src/helper/data.ts
+++ src/helper/data.ts
@@ -149,7 +149,7 @@ export const CUSTOMERS: ICustomer[] = [
 ];
 
 export const GROUPS: IGroup[] = [
-  { id: 1, title: 'Breads', persianTitle: 'نان تازه', persianSubtitle: 'خرید نان تازه و گرم', img: '/images/group/noon.png', enabled: true },
+  { id: 1, title: 'Breads', persianTitle: 'نان تازه', persianSubtitle: 'خرید نان تازه و گرم', img: '/images/group/bread.png', enabled: true },
   { id: 2, title: 'Market', persianTitle: 'سوپر مارکت', persianSubtitle: 'انتخاب و خرید مواد غذایی', img: '/images/group/shopping-cart.png', enabled: false },
   { id: 3, title: 'FastFoods', persianTitle: 'فست فود', persianSubtitle: 'سفارش غذا از فست فود و کافه', img: '/images/group/fast-food.png', enabled: false },
   { id: 4, title: 'Restaurants', persianTitle: 'رستوران', persianSubtitle: 'سفارش غذاهای ایرانی و محلی', img: '/images/group/resturant.png', enabled: false },
